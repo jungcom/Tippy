08793822278d7075cadeea08416a14e56322aec2
Anthony Lee <anthonylee@umass.edu>
2018-09-06 12:20:30 -0400

    Created new repository

--- Tippy/ViewController.swift
+++ Tippy/ViewController.swift
@@ -10,16 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipPercentageControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func tapped(_ sender: AnyObject) {
+        view.endEditing(true)
     }
-
-
+    
+    @IBAction func caculate(_ sender: Any) {
+        let tipPercentage = [0.15,0.18,0.2]
+        
+        let bill = (Double(billField.text!)) ?? 0
+        let tip = bill * tipPercentage[tipPercentageControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
 }
 
